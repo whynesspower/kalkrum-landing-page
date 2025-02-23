6efc1379e5fdecdc364a4e089ca59dc7885d22ca
Yashraj <yashrajshukla48@gmail.com>
2025-02-24 00:06:05 +0530

    feat(ui): adding posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,35 @@ import type { AppProps } from "next/app";
 import { AuthProvider } from "@saas-ui/auth";
 import { SaasProvider } from "@saas-ui/react";
 import { Layout } from "components/layout";
+import { PostHogProvider } from 'posthog-js/react';
+import { posthog } from '../lib/posthog';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { announcement, header, footer } = pageProps;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => posthog?.capture('$pageview');
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+  }, [router.events]);
 
   return (
     <SaasProvider theme={theme}>
       <AuthProvider>
-        <Layout
-          announcementProps={announcement}
-          headerProps={header}
-          footerProps={footer}
-        >
-          <Component {...pageProps} />
-        </Layout>
+        <PostHogProvider client={posthog}>
+          <Layout
+            announcementProps={announcement}
+            headerProps={header}
+            footerProps={footer}
+          >
+            <Component {...pageProps} />
+          </Layout>
+        </PostHogProvider>
       </AuthProvider>
     </SaasProvider>
   );
